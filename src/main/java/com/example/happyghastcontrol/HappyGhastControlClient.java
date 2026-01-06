d2459dfac141adf01d3ebc5de6332f244bdef35b
HMwancheng <HM_wancheng@outlook.com>
2026-01-06 18:39:35 +0000

    Fix compatibility issues by updating Fabric API to 0.136.1+1.21.8 and restoring KeyBinding usage

--- src/main/java/com/example/happyghastcontrol/HappyGhastControlClient.java
+++ src/main/java/com/example/happyghastcontrol/HappyGhastControlClient.java
@@ -3,25 +3,25 @@ package com.example.happyghastcontrol;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyMapping;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class HappyGhastControlClient implements ClientModInitializer {
-    public static KeyMapping ascendKey;
-    public static KeyMapping descendKey;
+    public static KeyBinding ascendKey;
+    public static KeyBinding descendKey;
 
     @Override
     public void onInitializeClient() {
         // Register key bindings
-        ascendKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        ascendKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.happyghastcontrol.ascend",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_SPACE,
                 "category.happyghastcontrol"
         ));
 
-        descendKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        descendKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.happyghastcontrol.descend",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_LEFT_CONTROL,
